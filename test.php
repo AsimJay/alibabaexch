f6c34e2f445d4c28f35563221053411d4859d005
AsimJay <asimjansoomro@gmail.com>
2024-12-14 14:12:00 +0500

    Added the test file to ensure the connection.

--- test.php
+++ test.php
@@ -1,7 +1,3 @@
 <?php
-    // include "./admin/connect.php";
-    for($i=0; $i <= 99; $i++){
-    $sql = "INSERT INTO number_table (number, amount) VALUES ($i, 500);";
-    $result = mysqli_query($con, $sql);
-    }
-?> 
+echo "Hello World";
+?>
